eee8c1194998b38c0d855df820ba1eadd995acd9
Cat Norris <cannenorris@yahoo.com>
2017-12-15 01:02:55 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
+def sort_array_asc(array)
+  new_array=array.sort
+  new_array
+end
 
+def sort_array_desc(array)
+  new_array=(array.sort).reverse
+  new_array
+end
+
+def reverse_array(array)
+  new_array=array.reverse
+  new_array
+end
+
+def sum_array (array)
+  array.inject (0){ |sum, x| sum + x }
+end
+
+def find_a (array)
+  array.select { | word | word.start_with? ('a') }
+end
+
+def swap_elements (array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def kesha_maker (array)
+  array.each do |element|
+    element[2]="$"
+  end
+end
